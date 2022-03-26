14309058a199b1546466c16d669813c0eb213521
timothydn2003 <87452000+timothydn2003@users.noreply.github.com>
2022-03-25 19:48:44 -0700

    preloader

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ var scroll = new SmoothScroll('a[href*="#"]');
 
 var loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
+    this.setTimeout(10000)
     loader.style.display = "none"
 })
+
