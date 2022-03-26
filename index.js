ee6fddacf3c4577f0746b0086db50d01cd56a048
timothydn2003 <87452000+timothydn2003@users.noreply.github.com>
2022-03-26 13:49:21 -0700

    timeout

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var scroll = new SmoothScroll('a[href*="#"]');
 
 var loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
-    this.setTimeout(10000)
     loader.style.display = "none"
 })
 
